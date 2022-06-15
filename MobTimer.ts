0806df1ad0806998dd752641138d286a3dc6cee0
ethanstrominger <ethanstrominger2@gmail.com>
2022-06-15 01:20:26 +0000

    Refactor started for time remaining descrease test
    Co-Authored-By: Ethan Strominger <ethanstrominger2@gmail.com>
    Co-Authored-By: Thad Kerosky <contact@gmail.com> thadk
    Co-Authored-By: David Sturgis <david-sturgis@pluralsight.com>
    Co-Authored-By: Olivier Chambon <olivier.chambon@gmail.com>
    Co-Authored-By: Rodrigo Passos <rodrigo@hellodative.com>
    Co-Authored-By: Dan Meegan <dn_meegan@yahoo.com>

--- MobTimer.ts
+++ MobTimer.ts
@@ -1,10 +1,30 @@
 import { Statuses } from "./Statuses";
 
 export class MobTimer {
+  timeRemaining: number = 0;
+
   start() {
-    this.status = Statuses.Running;
+    this._status = Statuses.Running;
+    this.timeRemaining = this.durationMinutes * 60;
+    this.timeRemainingString = this.format();
   }
+
   durationMinutes: number = 5;
-  timeRemainingString: string = "0:00";
-  status: Statuses = Statuses.Ready;
+  timeRemainingString: string = this.format();
+  _status: Statuses = Statuses.Ready;
+
+  private format(): string {
+    const date = new Date(0);
+    date.setSeconds(this.timeRemaining);
+    return date.toISOString().substring(14, 19)
+  }
+
+  decreaseSeconds(seconds:number){
+    this.timeRemaining = this.timeRemaining-seconds;
+    
+  }
+
+  get status() {
+    return this._status
+  }
 }
--- mobTimer.test.ts
+++ mobTimer.test.ts
@@ -17,7 +17,7 @@ test("Set duration to 3.5 minutes", () => {
 
 test("Get time string before started", () => {
   const mobTimer = new MobTimer();
-  expect(mobTimer.timeRemainingString).toEqual("0:00");
+  expect(mobTimer.timeRemainingString).toEqual("00:00");
 });
 
 test("Initial status - status is READY", () => {
@@ -31,12 +31,26 @@ test("Start timer - status is RUNNING", () => {
   expect(mobTimer.status).toEqual(Statuses.Running);
 });
 
-test.skip("Seconds remaining equals 0 before started", () => {
-});
-test.skip("Seconds remaing equals 5 * 60 after started", () => {})
-
-test.skip("Get time string before started", () => {
+test("Seconds remaining equals 0 before started", () => {
   const mobTimer = new MobTimer();
-  expect(mobTimer.timeRemainingString).toEqual("0:00");
+  expect(mobTimer.timeRemaining).toEqual(0);
 });
-test.skip("Get time string after started", () => {});
+
+test("Seconds remaing equals 5 * 60 after started", () => {
+  const mobTimer = new MobTimer();
+  mobTimer.start();
+  expect(mobTimer.timeRemaining).toEqual(5 * 60);
+})
+
+test("Get time string after started", () => {
+  const mobTimer = new MobTimer();
+  mobTimer.start();
+  expect(mobTimer.timeRemainingString).toEqual("05:00")
+});
+
+test("Time remaining decreases by one second", ()=>{
+  const mobTimer = new MobTimer();
+  mobTimer.start();
+  mobTimer.decreaseSeconds(1);
+  expect(mobTimer.timeRemaining).toEqual((5 * 60) - 1);
+})
